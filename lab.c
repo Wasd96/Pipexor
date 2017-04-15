ac6782ada20880dfc720a00e897e5025960b9d24
Alexander Volkov <wasd3680@yandex.ru>
2017-04-15 14:28:15 +0300

    Universality

--- lab.c
+++ lab.c
@@ -17,14 +17,15 @@ int main(int argc, char *argv[])
 	char buff[LENGTH];
 	int pipes[2][2];
 	int i = 0;
-	int fileout;
-	int key;
+	int fileout = 0;
+	int key = 0;
 	pid_t pid;
 	int oflags = O_RDWR | O_CREAT | O_TRUNC;
 
-	if (argc < 3 || argc > 3) {
+	if (argc != 7) {
 		strcpy(str, "Wrong number of arguments\n");
-		strcat(str, "Usage: crypt/encrypt <file>\n");
+		strcat(str, "Usage: <prog_src> <file_src> <file_out> ");
+		strcat(str, "<prog_key> <file_key> <file_key_out>\n");
 		write(2, str, strlen(str));
 		exit(1);
 	}
@@ -47,7 +48,7 @@ int main(int argc, char *argv[])
 		dup(pipes[INPUTFILE][1]);
 		close(pipes[INPUTFILE][0]);
 		close(pipes[INPUTFILE][1]);
-		i = execlp("cat", "cat", argv[2], NULL);
+		i = execlp(argv[1], argv[1], argv[2], NULL);
 		if (i == -1) {
 			strcpy(str, "Exec error\n");
 			write(2, str, strlen(str));
@@ -58,88 +59,53 @@ int main(int argc, char *argv[])
 	dup(pipes[INPUTFILE][0]);
 	close(pipes[INPUTFILE][0]);
 	close(pipes[INPUTFILE][1]);
-	if (strcmp(argv[1], "crypt") == 0) {
-		int entropy;
 
-		fileout = open("fileout", oflags, S_IWUSR | S_IRUSR);
-		key = open("key", oflags, S_IWUSR | S_IRUSR);
-		entropy = open("/dev/urandom", O_RDONLY);
-		if (fileout == -1 || key == -1 || entropy == -1) {
-			strcpy(str, "File open error\n");
+	pid = fork();
+	if (pid == -1) {
+		strcpy(str, "Fork error\n");
+		write(2, str, strlen(str));
+		exit(1);
+	} else if (pid == 0) {
+		close(1);
+		dup(pipes[KEYFILE][1]);
+		close(pipes[KEYFILE][1]);
+		i = execlp(argv[4], argv[4], argv[5], NULL);
+		if (i == -1) {
+			strcpy(str, "Exec error\n");
 			write(2, str, strlen(str));
-			exit(3);
+			exit(2);
 		}
-		while (nread = read(0, buff, LENGTH)) {
-			uchar crypted;
-			uchar randbuff[LENGTH];
-			uchar randbyte;
-			int nreadkey;
-
-			nreadkey = read(entropy, randbuff, nread);
-			if (nreadkey < nread) {
-				strcpy(str, "Random read error\n");
-				write(2, str, strlen(str));
-				exit(4);
-			}
-			for (i = 0; i < nread; i++) {
-				randbyte = randbuff[i] & 0xFF;
-				crypted = buff[i] ^ randbyte;
-				write(fileout, &crypted, 1); 
-				write(key, &randbyte, 1);
-			}
-		}
-		close(fileout);
-		close(key);
-		close(entropy);
-	} else if (strcmp(argv[1], "encrypt") == 0) {
-		key = open("key", O_RDONLY);
-		if (key == -1) {
-			strcpy(str, "key missing error\n");
-			write(2, str, strlen(str));
-			exit(3);
-		}
-		close(key);
-
-		pid = fork();
-		if (pid == -1) {
-			strcpy(str, "Fork error\n");
-			write(2, str, strlen(str));
-			exit(1);
-		} else if (pid == 0) {
-			close(1);
-			dup(pipes[KEYFILE][1]);
-			close(pipes[KEYFILE][1]);
-			i = execlp("cat", "cat", "key", NULL);
-			if (i == -1) {
-				strcpy(str, "Exec error\n");
-				write(2, str, strlen(str));
-				exit(2);
-			}
-		}
-
-		fileout = open("filesrc", oflags, S_IRUSR | S_IWUSR);
-		if (fileout == -1) {
-			strcpy(str, "File open error\n");
-			write(2, str, strlen(str));
-			exit(3);
-		}
-		while (nread = read(0, buff, LENGTH)) {
-			uchar encrypted;
-			uchar randbuff[LENGTH];
-			int nreadkey;
-
-			nreadkey = read(pipes[KEYFILE][0], randbuff, nread);
-			if (nreadkey < nread) {
-				strcpy(str, "Key read error\n");
-				write(2, str, strlen(str));
-				exit(4);
-			}
-			for (i = 0; i < nread; i++) {
-				encrypted = buff[i] ^ randbuff[i];
-				write(fileout, &encrypted, 1);
-			}
-		}
-		close(fileout);
 	}
+
+
+	fileout = open(argv[3], oflags, S_IWUSR | S_IRUSR);
+	key = open(argv[6], oflags, S_IWUSR | S_IRUSR);
+
+	if (fileout == -1 || key == -1) {
+		strcpy(str, "File open error\n");
+		write(2, str, strlen(str));
+		exit(3);
+	}
+	while (nread = read(0, buff, LENGTH)) {
+		uchar crypted;
+		uchar randbuff[LENGTH];
+		uchar randbyte;
+		int nreadkey;
+
+		nreadkey = read(pipes[KEYFILE][0], randbuff, nread);
+		if (nreadkey < nread) {
+			strcpy(str, "Random read error\n");
+			write(2, str, strlen(str));
+			exit(4);
+		}
+		for (i = 0; i < nread; i++) {
+			randbyte = randbuff[i] & 0xFF;
+			crypted = buff[i] ^ randbyte;
+			write(fileout, &crypted, 1);
+			write(key, &randbyte, 1);
+		}
+	}
+	close(fileout);
+	close(key);
 	return 0;
 }
